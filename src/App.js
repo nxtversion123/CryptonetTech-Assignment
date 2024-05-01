b5035e22fe6ae6049b06643f1ee1de97e766f161
Manoj Sharma <52129946+nxtversion123@users.noreply.github.com>
2024-05-02 01:56:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="" style={{background:'#ccdbbd', height:'50pc',}}>
+    <div className="" style={{background:'#ccdbbd', height:'45pc',}}>
     <Navbar />
           <Card  />
       <Fetch />
